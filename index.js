57df6a4b93b4a0df76309dde187527a0831004dc
Reene <67369326+Reene444@users.noreply.github.com>
2024-10-23 21:44:47 +0100

    update

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 export const ghostModel = require('./models/ghost_w_tophat-transformed.glb');
 export const innModel = require('./models/fantasy_game_inn2-transformed.glb');
-export const nightModel =require ('./models/enviroments/night.hdr');
